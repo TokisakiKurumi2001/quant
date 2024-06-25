0ff5bd99a7b7e1cfd4abb542db02c3a1b8a59a3b
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2024-06-25 15:50:10 +0700

    Metrics evaluation

---
+++ calc_rouge.py
@@ -0,0 +1,24 @@
+import evaluate
+import json
+
+TEST_FILE_PATH='data/cnn/test.jsonl'
+PREDICT_PATH='pred.jsonl'
+
+if __name__ == "__main__":
+    preds = []
+    with open(PREDICT_PATH) as fin:
+        for line in fin:
+            d = json.loads(line)
+            preds.append(d)
+
+    data = []
+    with open(TEST_FILE_PATH) as fin:
+        for line in fin:
+            d = json.loads(line)
+            data.append(d)
+
+    metrics = evaluate.load("metrics/rouge.py")
+    for d, p in zip(data, preds):
+        metrics.add_batch(predictions=p, references=d['output'])
+    result = metrics.compute()
+    print(result)
